7a9ab7e207bd7eec299b7c6b0e8cebe67cb1a11c
Berstanio <berstaniodev@gmail.com>
2026-03-15 11:59:16 +0100

    feat: Update to new IPC handler

--- src/main/java/io/github/berstanio/pymobiledevice3/ipc/PyMobileDevice3IPC.java
+++ src/main/java/io/github/berstanio/pymobiledevice3/ipc/PyMobileDevice3IPC.java
@@ -30,7 +30,7 @@ public class PyMobileDevice3IPC implements Closeable {
 
     private static final boolean DEBUG = System.getProperty("java.pymobiledevice3.debug") != null;
 
-    public static final int PROTOCOL_VERSION = 4;
+    public static final int PROTOCOL_VERSION = 5;
 
     private final int daemonProtocolVersion;
     private final Socket socket;
