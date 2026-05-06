d05b2e0ed09d4462db88c87bd15e9abf6f18489d
Berstanio <berstaniodev@gmail.com>
2026-05-06 23:25:00 +0200

    fix: Bump to new protocol version

--- src/main/java/io/github/berstanio/pymobiledevice3/ipc/PyMobileDevice3IPC.java
+++ src/main/java/io/github/berstanio/pymobiledevice3/ipc/PyMobileDevice3IPC.java
@@ -30,7 +30,7 @@ public class PyMobileDevice3IPC implements Closeable {
 
     private static final boolean DEBUG = System.getProperty("java.pymobiledevice3.debug") != null;
 
-    public static final int PROTOCOL_VERSION = 6;
+    public static final int PROTOCOL_VERSION = 7;
 
     private final int daemonProtocolVersion;
     private final Socket socket;
